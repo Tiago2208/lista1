d1cc6d45834abeb46a06913ea73412019f2e0ebc
Tiago Andrade <tiago22900@gmail.com>
2022-04-14 17:42:28 -0400

    Modificacoes

---
+++ src/Exercicio13A.java
@@ -0,0 +1,16 @@
+
+public class Exercicio13A {
+    public static void main(String[] args) {
+        int contador = 0;
+        for (int i = 2; i <= 4; i++) {
+            for (int j = 2; j <= 4; j++) {
+                for (int k = 4; k >= 2; k--) {
+                    System.out.println("" + i + j + k);
+                    contador += 1;
+                }
+            }
+        }
+        System.out.println("O número total de três digitos é: " + contador);
+    }
+
+}
--- src/Exercicio22.java
+++ src/Exercicio22.java
@@ -4,8 +4,20 @@ public class Exercicio22 {
     public static void main(String[] args) {
         Scanner leitor = new Scanner(System.in);
         System.out.println("Digite quantos segundos você deseja converter: ");
-        int segundos = leitor.nextInt();
-        
+        int tempoTotal = leitor.nextInt();
+
+        int horas;
+        int minutos;
+        int segundos;
+
+        minutos = (int) (tempoTotal / 60);
+        segundos = tempoTotal % 60;
+        horas = (int) minutos / 60;
+        minutos = minutos % 60;
+
+
+        System.out.println(horas + ":" + minutos + ":" + segundos);
+        leitor.close();
 
     }
     
