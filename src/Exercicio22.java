ca7210058417c72763a956ae5db97308bac8bc96
Tiago Andrade <tiago22900@gmail.com>
2022-04-21 13:54:02 -0400

    mudança

--- src/Exercicio22.java
+++ src/Exercicio22.java
@@ -5,20 +5,16 @@ public class Exercicio22 {
         Scanner leitor = new Scanner(System.in);
         System.out.println("Digite quantos segundos você deseja converter: ");
         int tempoTotal = leitor.nextInt();
-
         int horas;
         int minutos;
         int segundos;
-
         minutos = (int) (tempoTotal / 60);
         segundos = tempoTotal % 60;
         horas = (int) minutos / 60;
         minutos = minutos % 60;
-
-
         System.out.println(horas + ":" + minutos + ":" + segundos);
         leitor.close();
 
     }
-    
+
 }
